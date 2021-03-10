bb46d34b25f2152b22340a162ba1c5f918a4b0fe
Bruno Gomes <23224552+Lodek@users.noreply.github.com>
2021-03-10 18:16:16 -0300

    tests: immutable iterator fold tested and working

--- src/util.rs
+++ src/util.rs
@@ -1,25 +1,27 @@
 
 /// Defines a fold over an iterator using a `Fn` instead of `FnMut`.
-pub fn imutable_iterator_fold<F, U, V, I>(f: F, acc: V, mut iterator: I) -> V 
+pub fn immutable_iterator_fold<'a, F, U, V, I>(f: F, acc: V, mut iterator: I) -> V 
     where F: Fn(V, U) -> V,
-          I: Iterator<Item = U>
+          U: 'a,
+          I: Iterator<Item = U>,
 {
     match iterator.next() {
         Some(data) => {
             let acc = f(acc, data);
-            imutable_iterator_fold(f, acc, iterator)
+            immutable_iterator_fold(f, acc, iterator)
         }
         None => acc
     }
 }
 
 mod test {
+    use super::*;
 
     #[cfg(test)]
-    fn test_imutable_iterator_fold() {
-        let vales = vec![1,2,3];
+    fn test_immutable_iterator_fold() {
+        let values = vec![1,2,3];
         let folding = |acc, value| value + acc;
-        let result = imutable_iterator_fold(folding, 0, values.iter());
+        let result = immutable_iterator_fold(folding, 0, values.iter());
         assert_eq!(result, 6);
     }
 
